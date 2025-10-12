53ef2c9c7579f7f096d3232bf9f1afaa8b663d14
Elshadayyyyy <alemelshaday036@gmail.com>
2025-10-12 15:41:37 +0300

    finallll

--- src/pages/Servicespage.tsx
+++ src/pages/Servicespage.tsx
@@ -2,7 +2,7 @@ import { Button } from '../components/ui/button';
 import { Card, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
 import { CheckCircle, Zap, Users, Globe, Settings } from 'lucide-react';
 import Hero from "./../assets/servicepage/heroNew.png";
-import HeroMobile from "./../assets/servicepage/erpMobile.jpg";
+import HeroMobile from "./../assets/servicepage/heroMobile.png";
 import ERPImp from "./../assets/homepage/oddoNew.png";
 import customSoft from "./../assets/servicepage/customSoftware.png";
 import bussinesAuto from "./../assets/servicepage/businessAuto.png";
